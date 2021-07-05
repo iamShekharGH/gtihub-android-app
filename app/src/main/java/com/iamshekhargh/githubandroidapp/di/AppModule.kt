c3f01277eb09cf201f2889bfcef2e83e8981cb0f
iamShekharGH <shekhargh@shekhargh.com>
2021-07-05 14:43:26 +0530

    created retrofit object

--- app/src/main/java/com/iamshekhargh/githubandroidapp/di/AppModule.kt
+++ app/src/main/java/com/iamshekhargh/githubandroidapp/di/AppModule.kt
@@ -1,8 +1,16 @@
 package com.iamshekhargh.githubandroidapp.di
 
+import com.iamshekhargh.githubandroidapp.network.GithubApi
+import com.iamshekhargh.githubandroidapp.repo.GithubAppRepository
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
 
 /**
  * Created by <<-- iamShekharGH -->>
@@ -13,4 +21,33 @@ import dagger.hilt.components.SingletonComponent
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideRepoInstance() = GithubAppRepository()
+
+    // Room ----------------
+
+    // ----------------
+
+    // Retrofit -----------
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit =
+        Retrofit.Builder().client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(GithubApi.BASE_URL).build()
+
+    @Provides
+    @Singleton
+    fun provideGithubApi(retrofit: Retrofit) = retrofit.create(GithubApi::class.java)
+
+    // --------------------
+
 }
